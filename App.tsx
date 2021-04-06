9a3c1cbb743d4f912733adf35ed7ad083ef15901
hiji <shinya.hijiri@gmail.com>
2021-04-06 14:41:27 +0900

    add navigate

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import {createStackNavigator} from "@react-navigation/stack";
+import {Button, Text, View} from 'react-native';
+import {createStackNavigator, StackScreenProps} from "@react-navigation/stack";
 import {NavigationContainer} from "@react-navigation/native";
 
-function HomeScreen() {
+type StackParamList = {
+  Home: undefined,
+  Details: undefined
+};
+
+function HomeScreen({ navigation } : StackScreenProps<StackParamList, 'Home'>) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
     </View>
   );
 }
