5e83c5c0ea0eb4b4c32980c9a189abded56e10b3
hiji <shinya.hijiri@gmail.com>
2021-04-06 19:33:14 +0900

    add parameter

--- App.tsx
+++ App.tsx
@@ -5,31 +5,41 @@ import {NavigationContainer} from "@react-navigation/native";
 
 type StackParamList = {
   Home: undefined,
-  Details: undefined
+  Details: { itemId: number, otherParam?: string }
 };
 
-type HomeProps = StackScreenProps<StackParamList, 'Home'>;
-type DetailsProps = StackScreenProps<StackParamList, 'Details'>;
+type HomeScreenProps = StackScreenProps<StackParamList, 'Home'>;
+type DetailsScreenProps = StackScreenProps<StackParamList, 'Details'>;
 
-function HomeScreen({ navigation } : HomeProps) {
+function HomeScreen({ navigation } : HomeScreenProps) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
       <Button 
         title="Go to Details" 
-        onPress={() => navigation.navigate('Details')} 
+        onPress={() => {
+          navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'anything you want here',
+          });
+        }}
       />
     </View>
   );
 }
 
-function DetailsScreen({ navigation } : DetailsProps) {
+function DetailsScreen({ route, navigation } : DetailsScreenProps) {
+  const { itemId, otherParam } = route.params;
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
       <Button
         title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
+        onPress={() => navigation.push('Details', {
+          itemId: Math.floor(Math.random() * 100),
+        })}
       />
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
