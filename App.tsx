9ca33136314a7b779fb3d133ed8e47c2fd8f3d45
hiji <shinya.hijiri@gmail.com>
2021-04-06 14:51:18 +0900

    moving between screens

--- App.tsx
+++ App.tsx
@@ -8,19 +8,35 @@ type StackParamList = {
   Details: undefined
 };
 
-function HomeScreen({ navigation } : StackScreenProps<StackParamList, 'Home'>) {
+type HomeProps = StackScreenProps<StackParamList, 'Home'>;
+type DetailsProps = StackScreenProps<StackParamList, 'Details'>;
+
+function HomeScreen({ navigation } : HomeProps) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
-      <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
+      <Button 
+        title="Go to Details" 
+        onPress={() => navigation.navigate('Details')} 
+      />
     </View>
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({ navigation } : DetailsProps) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
     </View>
   );
 }
